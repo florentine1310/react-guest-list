94fdc9691a9239ac294918337c6cf5a7d6bfcc5b
Florentine Ramboeck <florentine.ramboeck@web.de>
2025-01-30 15:26:42 +0100

    Create basic html structure, consts and input fields

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import './App.css';
-import logo from './logo.svg';
+import { useState } from 'react';
 
 export default function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [isAttending, setIsAttending] = useState(false);
+  const guest = {
+    firstName: { firstName },
+    lastName: { lastName },
+    isAttending: { isAttending },
+  };
+  console.log(guest);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div title={`data-test-id="guest"`}>
+      <h1> My Guest List</h1>
+      <label>
+        First Name:
+        <input
+          name="firstName"
+          placeholder="Your First Name"
+          value={firstName}
+          onChange={(event) => {
+            setFirstName(event.currentTarget.value);
+          }}
+        />
+      </label>
+      <label>
+        Last Name:
+        <input
+          name="lastName"
+          placeholder="Your Last Name"
+          value={lastName}
+          onChange={(event) => {
+            setLastName(event.currentTarget.value);
+          }}
+        />
+      </label>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>is Attending</th>
+            <th> </th>
+          </tr>
+          <tr>
+            <td>{firstName}</td>
+            <td>{lastName}</td>
+            <td>
+              <input
+                name="isAttending"
+                type="checkbox"
+                checked={isAttending}
+                onChange={(event) => {
+                  setIsAttending(event.currentTarget.checked);
+                }}
+              />
+            </td>
+            <td>
+              <button>Remove</button>
+            </td>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }
